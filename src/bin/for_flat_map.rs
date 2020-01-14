0aae557df493346ba23dc283cc76a5d2a9b5f2cb
Damian J Pound <damian@thebestisp.com>
2020-01-14 13:53:55 -0600

    for_flat_map: Use Either to avoid a Box

--- src/bin/for_flat_map.rs
+++ src/bin/for_flat_map.rs
@@ -2,6 +2,9 @@ use rayon::prelude::*;
 // Heap allocations are expensive and reusing string buffers gets complicated with threads.
 // Probably more efficient on CPU cache since the stack has to already be in there.
 use arrayvec::ArrayString;
+// Either allows avoiding a box in the first flat_map,
+// though it doesn't make a noticeable difference to performance.
+use either::Either;
 use string_finder::{STRLEN, get_chars};
 
 fn main() {
@@ -11,19 +14,19 @@ fn main() {
 		// All lengths that will be tested.
 		(0..=STRLEN)
 			// For each STRLEN, get all possible iterations of that length
-			.flat_map(|strlen| -> Box<dyn Send+Iterator<Item=(usize, ArrayString<[_; 20]>)>> {
+			.flat_map(|strlen| /*-> Either<Too, Much>*/ {
 				//Zero length string is handled specially
-				if strlen==0 {return Box::new(std::iter::once((0, ArrayString::new())));}
+				if strlen==0 {return Either::Left(std::iter::once((0, ArrayString::new())));}
 
 				// Iterator of each letter in a string
-				Box::new(chars.clone().map(move |c| {
+				Either::Right(chars.clone().map(move |c| {
 					let mut string=ArrayString::new();
 					string.push(c);
 					(strlen-1, string)
 				}))
 			})
 			.par_bridge()
-			.flat_map(|(strlen, string)| {
+			.flat_map(|(strlen, string): (usize, ArrayString<[_; 20]>)| {
 				// Type must be specified, otherwise the compiler looks for a struct instead of trait.
 				let mut iter: Box<dyn Send+Iterator<Item=ArrayString<_>>>
 					=Box::new(std::iter::once(string));
